7313be797a3780626661dff55446bb2e4f5357f6
Alonso Auqui <andriod.pvp@gmail.com>
2024-09-23 14:14:05 -0500

    🚀 Proyecto FRONT-END | Autos T1

---
+++ src/main/java/pe/edu/cibertec/t1_auqui_frontend/T1AuquiFrontendApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t1_auqui_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1AuquiFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1AuquiFrontendApplication.class, args);
+	}
+
+}
